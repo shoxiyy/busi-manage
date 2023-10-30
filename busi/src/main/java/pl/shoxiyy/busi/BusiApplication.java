4ea684170af08c4c127cdfbd1665e0819627b44c
shoxiyy <107638546+shoxiyy@users.noreply.github.com>
2023-10-30 01:49:14 +0100

    First configuration and project structure

---
+++ busi/src/main/java/pl/shoxiyy/busi/BusiApplication.java
@@ -0,0 +1,13 @@
+package pl.shoxiyy.busi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusiApplication.class, args);
+    }
+
+}
---
+++ busi/src/test/java/pl/shoxiyy/busi/BusiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.shoxiyy.busi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
